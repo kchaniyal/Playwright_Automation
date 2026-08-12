b36564c283557d7a748c8d27b1ad0fc6189ca586
kchaniyal <50007682+kchaniyal@users.noreply.github.com>
2026-08-12 10:13:52 +0530

    Set headless mode to true in Playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -10,7 +10,7 @@ module.exports = defineConfig({
   },
   use: {
     browserName: 'chromium',
-    headless: false,
+    headless: true,
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
